d1c2463f83888db818fe9721e4d654b55c28e8f7
Amadeus Dias <amadeus@pop-os.localdomain>
2022-06-09 15:53:51 -0300

    completando a aula 9.3

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -2,5 +2,23 @@ require('../mocks/fetchSimulator');
 const { fetchCharacter } = require('../src/fetchCharacter');
 
 describe('Teste a função fetchCharacter', () => {
-  it('', () => {});
+  it('Verifica se o nome da personagem é Wonder Woman', async () => {
+    const character = await fetchCharacter(720);
+    expect(character.name).toBe('Wonder Woman');
+  });
+  it('Verifica se retorna erro ao executar a função sem parâmetro', async () => {
+    const failRequest = await fetchCharacter();
+    expect(failRequest).toEqual(new Error('You must provide an url'));
+  })
+  // testar direto no resulta das promises utilizando resolves e reject salva linhas de codigo, ja que o awaitr pode ser colocado na mesma linha do expect. É como se voce estivesse dizendo "espero receber que a função tal seja resolvida ou rejeitada dessa ou daquela forma".
+  it('Verifica se retorna \'Invalid id\' ao executar a função com parâmetro que não existe', async () => {
+    const response = await fetchCharacter('parâmetro qualquer');
+    expect(response).toBe('Invalid id');
+  });
+  it('Verifica se fetch é chamada com o endpoint correto', async () => {
+    const url = 'https://www.superheroapi.com/api.php/4192484924171229/720';
+    await fetchCharacter('720');
+    expect(fetch).toHaveBeenCalledTimes(4);
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
 });
